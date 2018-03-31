06472d038a7231da6872fe81f79024b7c0fea517
Seyitahmet Tuzcuoğlu <tuzcuoglu.seyitahmet@gmail.com>
2018-03-31 22:50:01 +0300

    add auth middleware

---
+++ routes/auth_middleware.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"gym-back/repositories"
+
+	"github.com/gin-gonic/gin"
+)
+
+// AuthMiddleware checks accesibility on protected routes
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Token")
+		token := repositories.FindUserByToken(tokenString)
+		if token.UserID == 0 {
+			c.AbortWithStatus(401)
+			return
+		}
+		c.Set("user", token.User)
+		c.Next()
+	}
+}

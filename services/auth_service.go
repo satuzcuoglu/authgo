6dccdca6d3e0285420b367ab666f5c0ee1a79252
Seyitahmet Tuzcuoğlu <tuzcuoglu.seyitahmet@gmail.com>
2018-03-31 13:16:04 +0300

    modify register method

--- services/auth_service.go
+++ services/auth_service.go
@@ -2,47 +2,49 @@ package services
 
 import (
 	"crypto/rand"
+	"errors"
 	"fmt"
 	"gym-back/database"
 	"gym-back/models"
 	"gym-back/repositories"
-	"log"
 	"time"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
 // CreateUser creates an user with hashed password
-func CreateUser(user *models.User) *models.User {
-	log.Println(user.Username)
-	log.Println(user.Password)
+func CreateUser(user *models.User) (*models.User, error) {
+	userByUsername := repositories.FindUserByUsername(user.Username)
+	if userByUsername.Username != "" {
+		return nil, errors.New("Username Already Taken")
+	}
+
+	userByEmail := repositories.FindUserByEmail(user.Email)
+	if userByEmail.Email != "" {
+		return nil, errors.New("Email Already in use")
+	}
 
 	user.Password = generateHashedPassword(user.Password)
 	repositories.CreateUser(user)
-	return user
+	return user, nil
 }
 
 // CreateToken creates an auth_token
-func CreateToken(username string, password string) *models.AuthToken {
+func CreateToken(username string, password string) (*models.AuthToken, error) {
 	db := database.GetConnection()
 	user := repositories.FindUserByUsername(username)
 	token := new(models.AuthToken)
 
-	log.Println(username)
-	log.Println(password)
-
 	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
-	log.Println(err)
 	if err != nil {
-		log.Println("Error")
-		return nil
+		return nil, err
 	}
 
 	token.Token = generateTokenString()
 	token.User = *user
 	token.ExpiresAt = time.Now().Add(72 * time.Hour)
 	db.Create(&token)
-	return token
+	return token, nil
 }
 
 func generateHashedPassword(password string) string {

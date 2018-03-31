a90d132ed5cc6563de15b5268e1d4009a2706d2d
Seyitahmet Tuzcuoğlu <tuzcuoglu.seyitahmet@gmail.com>
2018-03-31 13:15:35 +0300

    add cors middleware and move routes to gin

--- routes/router.go
+++ routes/router.go
@@ -3,14 +3,30 @@ package routes
 import (
 	"gym-back/controllers"
 
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
 )
 
 // InitRoutes initializes all routes for app
-func InitRoutes() *mux.Router {
-	router := mux.NewRouter()
+func InitRoutes() {
+	router := gin.Default()
+	router.Use(CORSMiddleware())
+	router.POST("/auth/register", controllers.Register)
+	router.POST("/auth/login", controllers.Login)
+	router.Run(":7000")
+}
 
-	router.HandleFunc("/auth/register", controllers.Register)
-	router.HandleFunc("/auth/login", controllers.Login)
-	return router
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }

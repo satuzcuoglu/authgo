7d436f6dfc831e651f1723f6790dcd75658883eb
Seyitahmet Tuzcuoğlu <tuzcuoglu.seyitahmet@gmail.com>
2018-03-31 23:34:12 +0300

    handle marshaling user model

--- models/user.go
+++ models/user.go
@@ -1,16 +1,27 @@
 package models
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 // User Model
 type User struct {
-	ID        uint       `json:"id"`
-	Username  string     `gorm:"unique_index" sql:"type:varchar(20)" json:"username"`
-	Password  string     `json:"password,omitempty"`
-	Firstname string     `json:"firstname"`
-	Lastname  string     `json:"lastname"`
-	Email     string     `gorm:"unique_index" sql:"type:varchar(50)" json:"email"`
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"updated_at"`
+	ID        uint       `json:"id" binding:"required"`
+	Username  string     `gorm:"unique_index" sql:"type:varchar(20)" json:"username" binding:"required"`
+	Password  string     `json:"password,omitempty" binding:"required"`
+	Firstname string     `json:"firstname" binding:"required"`
+	Lastname  string     `json:"lastname" binding:"required"`
+	Email     string     `gorm:"unique_index" sql:"type:varchar(50)" json:"email" binding:"required"`
+	CreatedAt time.Time  `json:"-"`
+	UpdatedAt time.Time  `json:"-"`
 	DeletedAt *time.Time `json:"deleted_at,omitempty"`
+}
+
+// MarshalJSON used for Marshaling User
+func (u User) MarshalJSON() ([]byte, error) {
+	type user User // prevent recursion
+	x := user(u)
+	x.Password = ""
+	return json.Marshal(x)
 }

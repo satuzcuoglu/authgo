2014ea16f183298e9a44794d941d21d26632a844
Seyitahmet Tuzcuoğlu <tuzcuoglu.seyitahmet@gmail.com>
2018-03-31 22:51:54 +0300

    add auth token repository tests

---
+++ repositories/test/auth_token_repository_test.go
@@ -0,0 +1,45 @@
+package test
+
+import (
+	"gym-back/models"
+	"gym-back/repositories"
+	"testing"
+	"time"
+
+	"github.com/icrowley/fake"
+)
+
+var token = "qwerty12345"
+var userData *models.User
+
+func createTestUser() *models.User {
+	user := new(models.User)
+	user.Username = fake.UserName()
+	user.Firstname = fake.FirstName()
+	user.Lastname = fake.LastName()
+	user.Password = fake.SimplePassword()
+	user.Email = fake.EmailAddress()
+	repositories.CreateUser(user)
+	userData = user
+	return user
+}
+
+func TestCreateAuthToken(t *testing.T) {
+	authToken := new(models.AuthToken)
+	authToken.Token = token
+	authToken.User = *createTestUser()
+	authToken.CreatedAt = time.Now().Add(72 * time.Hour)
+	repositories.CreateAuthToken(authToken)
+
+	if &authToken.User == nil {
+		t.Error("Token doesnt have user info ")
+	}
+}
+
+func TestFindUserByToken(t *testing.T) {
+	authToken := repositories.FindUserByToken(token)
+	userInfo := &authToken.User
+	if userInfo == nil {
+		t.Error("Token doesnt have user info")
+	}
+}

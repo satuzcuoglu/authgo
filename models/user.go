893cd17dcf70723a577e903ebc860827ff32d7a7
Seyitahmet Tuzcuoğlu <tuzcuoglu.seyitahmet@gmail.com>
2018-04-02 00:46:46 +0300

    add binding params to user model

--- models/user.go
+++ models/user.go
@@ -7,15 +7,16 @@ import (
 
 // User Model
 type User struct {
-	ID        uint       `json:"id" binding:"required"`
-	Username  string     `gorm:"unique_index" sql:"type:varchar(20)" json:"username" binding:"required"`
-	Password  string     `json:"password,omitempty" binding:"required"`
-	Firstname string     `json:"firstname" binding:"required"`
-	Lastname  string     `json:"lastname" binding:"required"`
-	Email     string     `gorm:"unique_index" sql:"type:varchar(50)" json:"email" binding:"required"`
-	CreatedAt time.Time  `json:"-"`
-	UpdatedAt time.Time  `json:"-"`
-	DeletedAt *time.Time `json:"deleted_at,omitempty"`
+	ID          uint        `json:"-" binding:"required"`
+	Username    string      `gorm:"unique_index" sql:"type:varchar(20)" json:"username" binding:"required"`
+	Password    string      `json:"password,omitempty" binding:"required"`
+	Firstname   string      `json:"firstname" binding:"required"`
+	Lastname    string      `json:"lastname" binding:"required"`
+	Email       string      `gorm:"unique_index" sql:"type:varchar(50)" json:"email" binding:"required"`
+	Authorities []Authority `json:"authorities" gorm:"auto_preload;many2many:user_authorities"`
+	CreatedAt   time.Time   `json:"-"`
+	UpdatedAt   time.Time   `json:"-"`
+	DeletedAt   *time.Time  `json:"deleted_at,omitempty"`
 }
 
 // MarshalJSON used for Marshaling User
